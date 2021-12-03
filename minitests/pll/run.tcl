c0f0a6ad4e3b5c0d7bf51fbb1edd641b92c19013
Miodrag Milanovic <mmicko@gmail.com>
2021-12-03 10:59:52 +0100

    update pll minitest

--- minitests/pll/run.tcl
+++ minitests/pll/run.tcl
@@ -1,3 +1,3 @@
 import_device eagle_s20.db -package BG256
 read_pnl pll.pnl
-bitgen -bit "pll.bit" -version 0X00 -g ucode:000000000000000000000000 -info -log_file dummy.log
+bitgen -bit "pll.bit" -version 0X00 -g ucode:000000000000000000000000 -info -log_file pll.log -fuse pll.fuse
